71b3ee1635f23a9d4bc89bd24888b5a20701e65f
Vivian Lunnikivi <vivian.lunnikivi@tut.fi>
2019-02-27 03:32:56 +0200

    Add example API call and fix too bright visuals.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <h1>The Weather App</h1>
         </header>
 
-        <div class="flex-container">
+        <div className="flex-container">
           <InteractiveBox title="Search city temperatures" btnTitle="Search" displayComponent={<InputBlock />} />
           <InteractiveBox title="Temperature" btnTitle="Save" displayComponent={<DisplayBlock />} />
           <InteractiveBox title="Saved cities" btnTitle="Display" displayComponent={<ListingBlock />} />
@@ -18,7 +18,7 @@ class App extends Component {
         </div>
 
         <div className={"bgImgDiv"}>
-          <img src={flowers} className={"bgImg"}/>
+          <img src={flowers} className={"bgImg"} alt={"Footer decorator with flowers in it."}/>
         </div>
       </div>
     );
@@ -30,8 +30,10 @@ class InteractiveBox extends React.Component {
     return (
       <div className={"container"}>
         <h2>{this.props.title}</h2>
+
         {this.props.displayComponent}
-        <button className={"btn"}>{this.props.btnTitle}</button>
+
+        <DropDownBtn btnTitle={this.props.btnTitle} />
       </div>
     );
   }
@@ -63,6 +65,34 @@ function ListingBlock() {
 
 function EmptyBlock() {
   return (<div className={"container"} />);
+}
+
+class DropDownBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.btnTitle = props.btnTitle;
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const httpObj = new XMLHttpRequest();
+    const url = 'data/2.5/weather/?q=Engelskirchen&appid=1612250bcac7dd8271eb41e7a93c97de';
+
+    httpObj.open("GET", url, true)
+    httpObj.onload = function(){
+      console.log(httpObj.response);
+    };
+    httpObj.send(null);
+  }
+
+  render() {
+    return (
+      <button className={"btn"} onClick={this.handleClick}>
+        {this.btnTitle}
+      </button>
+    );
+  }
 }
 
 export default App;
