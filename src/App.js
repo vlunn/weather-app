0c47bf0c9e72fa29656d905866d75be893728ffb
Vivian Lunnikivi <vivian.lunnikivi@tut.fi>
2019-02-28 05:51:51 +0200

    Display temperature for searched city in UI.

--- src/App.js
+++ src/App.js
@@ -24,26 +24,41 @@ class InteractiveContents extends React.Component {
   constructor(props) {
     super(props);
     this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleTemperatureChange = this.handleTemperatureChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {city: ''};
+    this.state = {city: '', temperature: ''};
   }
 
-  handleCityChange(event) {
-    this.setState({city: event});
+  handleCityChange(newCity) {
+    this.setState({city: newCity});
+  }
+
+  handleTemperatureChange(newTemperature) {
+    this.setState({temperature: newTemperature});
   }
 
   handleSubmit(event) {
     const city = this.state.city;
     const url = 'data/2.5/weather/?q=' + city + '&appid=1612250bcac7dd8271eb41e7a93c97de';
-
     const httpObj = new XMLHttpRequest();
-    httpObj.open("GET", url, true);
+    let fetchedTemperature;
+
+    httpObj.open("GET", url, false);
+
     httpObj.onload = function(){
-      console.log(httpObj.response);
+      if (httpObj.status === 200) {
+        const parsed = JSON.parse(httpObj.response);
+        fetchedTemperature = parsed.main.temp;
+      }
+      else {
+        console.log("Something went wrong. Did you remember to type in a city name before search?");
+      }
     };
+
     httpObj.send(null);
 
     event.preventDefault();
+    this.setState({temperature: fetchedTemperature});
   }
 
   render() {
@@ -59,7 +74,10 @@ class InteractiveContents extends React.Component {
 
         <DisplayBox
           title="Temperature"
-          btnTitle="Save" />
+          btnTitle="Save"
+          changeFunction={this.handleTemperatureChange}
+          temperature={this.state.temperature}
+          city={this.state.city} />
 
         <ListingBox
           title="Stored cities"
@@ -73,7 +91,6 @@ class InteractiveContents extends React.Component {
 class SearchCityBox extends React.Component {
   constructor(props) {
     super(props);
-    this.btnTitle = props.btnTitle;
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -101,37 +118,15 @@ class SearchCityBox extends React.Component {
 }
 
 class DisplayBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.btnTitle = props.btnTitle;
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    const city = this.state.value;
-    const url = 'data/2.5/weather/?q=' + city + '&appid=1612250bcac7dd8271eb41e7a93c97de';
-
-    const httpObj = new XMLHttpRequest();
-    httpObj.open("GET", url, true);
-    httpObj.onload = function(){
-      console.log(httpObj.response);
-    };
-    httpObj.send(null);
-
-    event.preventDefault();
-  }
-
   render() {
+    let output = (this.props.temperature !== '') ?
+      <p>City: {this.props.city}<br/>temperature: {this.props.temperature} F</p> :
+      <p>Search for a city!</p>;
+
     return (
       <div className={'container'}>
         <h2>{this.props.title}</h2>
+        {output}
       </div>
     );
   }
