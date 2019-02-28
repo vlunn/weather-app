f13f95860b0e4d31dd43aaa1fc58cdff95d2013d
Vivian Lunnikivi <vivian.lunnikivi@tut.fi>
2019-02-28 03:59:24 +0200

    Implement search for city by user's input

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ class App extends Component {
           <h1>The Weather App</h1>
         </header>
 
-        <div className="flex-container">
-          <InteractiveBox title="Search city temperatures" btnTitle="Search" displayComponent={<InputBlock />} />
-          <InteractiveBox title="Temperature" btnTitle="Save" displayComponent={<DisplayBlock />} />
-          <InteractiveBox title="Saved cities" btnTitle="Display" displayComponent={<ListingBlock />} />
-          <EmptyBlock />
-        </div>
+        <InteractiveContents/>
 
         <div className={"bgImgDiv"}>
           <img src={flowers} className={"bgImg"} alt={"Footer decorator with flowers in it."}/>
@@ -25,74 +20,136 @@ class App extends Component {
   }
 }
 
-class InteractiveBox extends React.Component {
+class InteractiveContents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {city: ''};
+  }
+
+  handleCityChange(event) {
+    this.setState({city: event});
+  }
+
+  handleSubmit(event) {
+    const city = this.state.city;
+    const url = 'data/2.5/weather/?q=' + city + '&appid=1612250bcac7dd8271eb41e7a93c97de';
+
+    const httpObj = new XMLHttpRequest();
+    httpObj.open("GET", url, true);
+    httpObj.onload = function(){
+      console.log(httpObj.response);
+    };
+    httpObj.send(null);
+
+    event.preventDefault();
+  }
+
   render() {
+    const parentCity = this.state.city;
     return (
-      <div className={"container"}>
-        <h2>{this.props.title}</h2>
+      <div className="flex-container">
+        <SearchCityBox
+          title="Search city temperatures"
+          btnTitle="Search"
+          changeFunction={this.handleCityChange}
+          submitFunction={this.handleSubmit}
+          city={parentCity} />
 
-        {this.props.displayComponent}
+        <DisplayBox
+          title="Temperature"
+          btnTitle="Save" />
 
-        <DropDownBtn btnTitle={this.props.btnTitle} />
+        <ListingBox
+          title="Stored cities"
+          btnTitle="Display" />
+        <EmptyBlock />
       </div>
     );
   }
 }
 
-function DisplayBlock() {
-  return (
-    <div>
-      <p>-- Lämpötilanäyttö --</p>
-    </div>
-  );
-}
-
-function InputBlock() {
-  return (
-    <div>
-      <p>-- Input-kenttä --</p>
-    </div>
-  );
-}
-
-function ListingBlock() {
-  return (
-    <div>
-      <p>-- Listaus --</p>
-    </div>
-  );
-}
-
-function EmptyBlock() {
-  return (<div className={"container"} />);
-}
-
-class DropDownBtn extends React.Component {
+class SearchCityBox extends React.Component {
   constructor(props) {
     super(props);
     this.btnTitle = props.btnTitle;
-
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleClick() {
-    const httpObj = new XMLHttpRequest();
-    const url = 'data/2.5/weather/?q=Engelskirchen&appid=1612250bcac7dd8271eb41e7a93c97de';
+  handleChange(event) {
+    this.props.changeFunction(event.target.value);
+  }
 
-    httpObj.open("GET", url, true)
-    httpObj.onload = function(){
-      console.log(httpObj.response);
-    };
-    httpObj.send(null);
+  handleSubmit(event) {
+    this.props.submitFunction(event);
   }
 
   render() {
     return (
-      <button className={"btn"} onClick={this.handleClick}>
-        {this.btnTitle}
-      </button>
+      <div className={'container'}>
+        <h2>{this.props.title}</h2>
+        Type in a city:
+        <fieldset className={'inputGroup'}>
+          <input type="text" value={this.props.city} onChange={this.handleChange} /><br/>
+          <input type="submit" value={this.props.btnTitle} className={'btn'} onClick={this.handleSubmit}/>
+        </fieldset>
+      </div>
     );
   }
+}
+
+class DisplayBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.btnTitle = props.btnTitle;
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const city = this.state.value;
+    const url = 'data/2.5/weather/?q=' + city + '&appid=1612250bcac7dd8271eb41e7a93c97de';
+
+    const httpObj = new XMLHttpRequest();
+    httpObj.open("GET", url, true);
+    httpObj.onload = function(){
+      console.log(httpObj.response);
+    };
+    httpObj.send(null);
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className={'container'}>
+        <h2>{this.props.title}</h2>
+      </div>
+    );
+  }
+}
+
+class ListingBox extends React.Component {
+  render() {
+    return (
+      <div className={"container"}>
+        <h2>{this.props.title}</h2>
+        <div>-- Listaus --</div>
+      </div>
+    );
+  }
+}
+
+function EmptyBlock() {
+  return (<div className={"container"} />);
 }
 
 export default App;
